5896bba82c06056339a89f0700edf31aacb22cd2
lucdev <me@lucdev.net>
2023-06-25 12:55:59 -0300

    Adjust mutate type

--- fe/src/utils/loginAction.ts
+++ fe/src/utils/loginAction.ts
@@ -1,19 +1,10 @@
-import Router from "next/router";
 import { commonApiFetch } from "./api";
 import { ApiStatusMessage } from "@/types/Api";
 import { UserWithPassword } from "@/types/User";
-import { awaitCallbackIfNeeded } from "./awaitCallbackIfNeeded";
 
-export const loginAction = async (
-  mutate: () => void | Promise<unknown>,
-  userData: Partial<UserWithPassword>
-) => {
-  const result = await commonApiFetch<ApiStatusMessage>("login", {
+export async function loginAction(userData: Partial<UserWithPassword>) {
+  return await commonApiFetch<ApiStatusMessage>("login", {
     method: "POST",
     body: JSON.stringify(userData)
   });
-
-  await awaitCallbackIfNeeded(mutate);
-  console.log(result);
-  Router.replace("/");
-};
+}
--- fe/src/utils/logoutAction.ts
+++ fe/src/utils/logoutAction.ts
@@ -1,14 +1,8 @@
-import Router from "next/router";
 import { commonApiFetch } from "./api";
 import { ApiStatusMessage } from "@/types/Api";
-import { awaitCallbackIfNeeded } from "./awaitCallbackIfNeeded";
 
-export const logoutAction = async (mutate: () => void | Promise<unknown>) => {
-  const result = await commonApiFetch<ApiStatusMessage>("logout", {
+export async function logoutAction() {
+  return await commonApiFetch<ApiStatusMessage>("logout", {
     method: "POST"
   });
-
-  await awaitCallbackIfNeeded(mutate);
-  console.log(result);
-  Router.replace("/");
-};
+}
