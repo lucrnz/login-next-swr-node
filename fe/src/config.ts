ea8f30f224ee3a4d692446a301144cf3274ebafd
lucdev <me@lucdev.net>
2023-06-25 12:48:50 -0300

    Add default route for logged in

--- fe/src/config.ts
+++ fe/src/config.ts
@@ -3,3 +3,5 @@ import { Inter } from "next/font/google";
 export const defaultFont = Inter({ subsets: ["latin"] });
 
 export const APP_NAME = "Notes App";
+
+export const defaultPageLoggedIn = "/notes";
