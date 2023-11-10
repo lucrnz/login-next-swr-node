daa2f0ede067c86960e73501d6e8d98d900ee6e1
lucdev <me@lucdev.net>
2023-11-10 14:26:24 -0300

    nix: Bump Node to v20

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     in {
       devShells.default = pkgs.mkShell {
         buildInputs = [
-          pkgs.nodejs-18_x
+          pkgs.nodejs_20
           pkgs.nodePackages.npm
         ];
       };
